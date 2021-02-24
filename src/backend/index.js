14512059a76584e460152245ed08bba622fdcb5d
Marcelo Casamassa <marcelocasamassa@gmaile.com>
2021-02-24 20:57:25 -0300

    First commit

---
+++ src/backend/index.js
@@ -0,0 +1,12 @@
+const { ipcMain } = require('electron')
+
+ipcMain.on('process-subtitles', (event, paths) => {
+    console.log(paths)
+
+    event.reply('process-subtitles', [
+        { name: 'i', amount: 1234 },
+        { name: 'you', amount: 900 },
+        { name: 'he', amount: 853 },
+        { name: 'she', amount: 541 }
+    ])
+})
