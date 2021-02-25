ef976dc4948510b5331cda22f2118119fc3fb442
Marcelo Casamassa <marcelocasamassa@gmaile.com>
2021-02-25 09:57:43 -0300

    Function to read files uploaded

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,12 +1,15 @@
 const { ipcMain } = require('electron')
+const pathsToRows = require('./pathsToRows')
 
 ipcMain.on('process-subtitles', (event, paths) => {
-    console.log(paths)
-
-    event.reply('process-subtitles', [
-        { name: 'i', amount: 1234 },
-        { name: 'you', amount: 900 },
-        { name: 'he', amount: 853 },
-        { name: 'she', amount: 541 }
-    ])
+    pathsToRows(paths)
+        .then(rows => console.log(rows))
+        .then(() => {
+            event.reply('process-subtitles', [
+                { name: 'i', amount: 1234 },
+                { name: 'you', amount: 900 },
+                { name: 'he', amount: 853 },
+                { name: 'she', amount: 541 }
+            ])
+        })
 })
